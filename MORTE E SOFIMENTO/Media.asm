dbab3bae3aadbb6066ac34073abaed298aa7a9c3
xTvini <142419660+xTvini@users.noreply.github.com>
2024-10-24 08:38:13 -0300

    Add files via upload

---
+++ MORTE E SOFIMENTO/Media.asm
@@ -0,0 +1,18 @@
+.data
+
+.text
+li $v0,5
+syscall
+move $s0,$v0
+
+li $v0,5
+syscall
+move $s1,$v0
+
+add $t0,$s0,$s1 # adiciona s0 e s2 no valor t0
+li $t1,2 #inicia o valor t1 como 2
+div $t0,$t0,$t1 #divide a armazena no valor t0
+
+li  $v0,1
+move $a0,$t0
+syscall
